cdd58c00bf95553f6dabb4c6dc6678e3a04a9c97
Yamaha <144177603+YamahaCoded@users.noreply.github.com>
2024-06-06 12:57:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ class MyClient(discord.Client):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
 
     async def on_message(self, message):
-
         if message.author.id == self.user.id:
             return
 
@@ -23,9 +22,9 @@ class MyClient(discord.Client):
             messages.append({'role': 'assistant', 'content': assistant_reply})
 
             history_limit = 3
+            
             if(len(messages) > history_limit):
                 messages.pop(0)
-
 
 intents = discord.Intents.default()
 intents.message_content = True
